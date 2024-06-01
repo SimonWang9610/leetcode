bad3f3f098732efb2d3ec68921b1ffe5d13dfb53
SimonWang9610 <dengpan9610.wang@gmail.com>
2024-06-01 19:14:42 -0400

    mark *979*

---
+++ typescript/tree/distribute_coins_bt_979.ts
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/distribute-coins-in-binary-tree/description/
+
+import { TreeNode } from "./tree_node";
+
+/**
+ *
+ * 1.. if a node needs a coin, the coin:
+ *  - either pass through one of its child nodes (contribution from its descendants)
+ *  - or pass through its parent node (contribution from its parent or ancestors)
+ *
+ * 2. if a node has more than 1 coin, it will move all extra coins to its parent node,
+ *  so its parent will decide how to distribute the extra coins
+ *
+ */
+function distributeCoins(root: TreeNode | null): number {
+  if (!root) {
+    return 0;
+  }
+
+  let moves = 0;
+
+  function balance(node: TreeNode | null): number {
+    if (!node) {
+      return 0;
+    }
+
+    let leftBalance = balance(node.left);
+    let rightBalance = balance(node.right);
+
+    let currentBalance = leftBalance + rightBalance + node.val - 1;
+
+    moves += Math.abs(currentBalance);
+
+    return currentBalance;
+  }
+
+  balance(root);
+
+  return moves;
+}
---
+++ typescript/tree/find_elements_contaminated_bt_1261.ts
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/description/
+
+import { TreeNode } from "./tree_node";
+
+/**
+ * 1. all elements of the recovered tree are unique.
+ */
+class FindElements {
+  elements: Set<number>;
+  recovered: TreeNode | null;
+  constructor(root: TreeNode | null) {
+    let elements = new Set<number>();
+
+    function recover(node: TreeNode | null) {
+      if (!node) {
+        return;
+      }
+
+      if (node.left) {
+        node.left.val = (node.val << 1) + 1;
+      }
+
+      if (node.right) {
+        node.right.val = (node.val + 1) << 1;
+      }
+
+      elements.add(node.val);
+
+      recover(node.left);
+      recover(node.right);
+    }
+
+    if (root) {
+      root.val = 0;
+    }
+
+    recover(root);
+
+    this.elements = elements;
+    this.recovered = root;
+  }
+
+  find(target: number): boolean {
+    return this.elements.has(target);
+  }
+}
